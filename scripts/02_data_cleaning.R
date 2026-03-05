1dc8060e410a3c483700536a2dfe249708dc906c
SAYANA V A <assayana03@gmail.com>
2026-03-05 15:03:20 +0530

    Update 02_data_cleaning.R

--- scripts/02_data_cleaning.R
+++ scripts/02_data_cleaning.R
@@ -1,2 +1,11 @@
  data <- na.omit(data)
  data
+ data$year <- as.numeric(data$year)
+
+ data$price <- as.numeric(data$sellingprice)
+
+ data$odometer <- as.numeric(data$odometer)
+
+ data$make <- as.factor(data$make)
+
+ data$model <- as.factor(data$model)
