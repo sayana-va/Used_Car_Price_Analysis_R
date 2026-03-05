78e7caa848ef8e1918e27cb41f422e65aafec3b5
SAYANA V A <assayana03@gmail.com>
2026-03-05 15:09:22 +0530

    Update 03_exploratory_analysis.R

--- scripts/03_exploratory_analysis.R
+++ scripts/03_exploratory_analysis.R
@@ -1,1 +1,12 @@
+library(dplyr)
+brand_sales <- data %>%
+  count(make) %>%
+  arrange(desc(n))
 
+ head(brand_sales)
+
+avg_price <- data %>%
+  group_by(make) %>%
+  summarise(avg_price = mean(price, na.rm = TRUE))
+
+avg_price
