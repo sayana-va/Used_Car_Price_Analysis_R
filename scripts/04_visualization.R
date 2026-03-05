f809008588b1b89326faffa790d08cd47ad2cbc4
SAYANA V A <assayana03@gmail.com>
2026-03-05 15:24:44 +0530

    Update 04_visualization.R

--- scripts/04_visualization.R
+++ scripts/04_visualization.R
@@ -1,1 +1,15 @@
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(brand_sales, aes(x = reorder(make, n), y = n)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  coord_flip() +
+  labs(title = "Top Selling Car Brands",
+       x = "Brand",
+       y = "Number of Cars Sold")
 
+
+ggplot(data, aes(x = odometer, y = sellingprice)) +
+  geom_point(color = "red") +
+  labs(title = "Car Price vs Mileage",
+       x = "Mileage",
+       y = "Price")
