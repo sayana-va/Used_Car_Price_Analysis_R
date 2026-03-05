91877e54b5dfce72f1262add81d0d03e028d7d96
SAYANA V A <assayana03@gmail.com>
2026-03-05 15:33:41 +0530

    Update libraries.R

--- scripts/libraries.R
+++ scripts/libraries.R
@@ -1,5 +1,13 @@
+# Install packages (run once if not installed)
 install.packages("tidyverse")
+install.packages("readr")
 install.packages("ggplot2")
 install.packages("dplyr")
-install.packages("readr")
 install.packages("lubridate")
+
+# Load libraries
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
