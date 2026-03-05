d0387f7835ed62c69586b2dc55a402708104a7b5
SAYANA V A <assayana03@gmail.com>
2026-03-05 14:55:03 +0530

    Update 01_data_loading.R

--- scripts/01_data_loading.R
+++ scripts/01_data_loading.R
@@ -1,1 +1,13 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(lubridate)
 
+data <- read.csv(file.choose(), stringsAsFactors = FALSE)
+
+head(data)
+str(data)
+summary(data)
+dim(data)
+names(data)
